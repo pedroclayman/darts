cee7ac9d379f5944cef9e6badd8066bdbe610494
pedroclayman <petermihalik@MacPedro.local>
2015-01-23 00:53:30 +0100

    added test for the game service

--- tests/services/game.spec.js
+++ tests/services/game.spec.js
@@ -1,11 +1,22 @@
 'use strict';
 
 describe('game', function() {
+  var game;
+
   beforeEach(function() {
     module('darts');
   });
 
-  it('should work', function() {
+  beforeEach(function() {
+    inject([
+      'game',
+      function(gameInj) {
+        game = gameInj;
+      }
+    ]);
+  });
 
+  it('should expose a method "makeMove"', function() {
+    expect(game.makeMove).toBeDefined();
   });
 });
