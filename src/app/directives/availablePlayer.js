95e8a481d798009bc4473b459e7576b5b205c390
pedroclayman <petermihalik@MacPedro.local>
2015-01-31 01:00:21 +0100

    styling and available players

--- src/app/directives/availablePlayer.js
+++ src/app/directives/availablePlayer.js
@@ -5,7 +5,12 @@ angular.module('darts').directive('availablePlayer', [
   function availablePlayer() {
     return {
       restrict: 'A',
-      template: '<div><img gravatar-src="player.email()" gravatar-size="50"><span>{{player.name()}}</span></div>'
+      template: '<div ng-class="{\'in-game\' : player.isInGame === true}"><img ng-click="toggleIsInGame()" gravatar-src="player.email()" gravatar-size="50"><span>{{player.name()}}</span></div>',
+      link: function(scope, element, attrs) {
+        scope.toggleIsInGame = function() {
+          scope.player.isInGame = !scope.player.isInGame;
+        };
+      }
     }
   }
 ]);
